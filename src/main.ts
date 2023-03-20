6026bacb85f1588d3ff08078b73dae24e303e64e
Vivek <vlakshmanan@alcion.ai>
2023-03-19 23:42:03 -0700

    Fix linting

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,43 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
-import { run, workflowRunStatus } from './run';
-
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import {run, workflowRunStatus} from './run'
 
 function mustGetEnvOrInput(envVar: string, inputName: string): string {
-  return process.env[envVar] ?? core.getInput(inputName, {required: true}); 
+  return process.env[envVar] ?? core.getInput(inputName, {required: true})
 }
 
-async function main() {
+async function main(): Promise<void> {
   const {
-    repo: { owner, repo },
-    payload,
-  } = github.context;
-  const lastSuccessfulRun = core.getInput("last-successful-run-id", {required: false});
-  run({
-    owner: owner,
-    repo: repo, 
-    githubToken: core.getInput('access-token'),
-    currentWorkflowRunId: Number(mustGetEnvOrInput("GITHUB_RUN_ID", "workflow-run-id")),
-    payload: payload,
-    limitToPreviousSuccessfulRunCommit: core.getBooleanInput("limit-to-previous-successful-run-commit", {required: false}),
-    lastSuccessfulRunId: lastSuccessfulRun ? Number(lastSuccessfulRun): undefined,
-    status: core.getInput('status-of-workflows-to-cancel', {required: true}) as workflowRunStatus,
-    dryRun: core.getBooleanInput('dry-run', {required: true}),
+    repo: {owner, repo},
+    payload
+  } = github.context
+  const lastSuccessfulRun = core.getInput('last-successful-run-id', {
+    required: false
   })
-    .then(() => core.info('Workflow cancel run completed.'))
-    .catch((e: any) => core.setFailed(e.message));
+  try {
+    await run({
+      owner,
+      repo,
+      githubToken: core.getInput('access-token'),
+      currentWorkflowRunId: Number(
+        mustGetEnvOrInput('GITHUB_RUN_ID', 'workflow-run-id')
+      ),
+      payload,
+      limitToPreviousSuccessfulRunCommit: core.getBooleanInput(
+        'limit-to-previous-successful-run-commit',
+        {required: false}
+      ),
+      lastSuccessfulRunId: lastSuccessfulRun
+        ? Number(lastSuccessfulRun)
+        : undefined,
+      status: core.getInput('status-of-workflows-to-cancel', {
+        required: true
+      }) as workflowRunStatus,
+      dryRun: core.getBooleanInput('dry-run', {required: true})
+    })
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
 }
 
 main()
